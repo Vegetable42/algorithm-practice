21ef32fba04cec652883dabcabeb7ba2ad351369
Tomáš Klásek <tom-klasek@seznam.cz>
2025-10-23 22:40:35 +0200

    New selectsort and bubblesort code

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -1,9 +1,14 @@
-arr = [64,2,15,3,2,1,2,3,5,7,8,4454,45]
-l = len(arr)
-for i in range(l):
-	for j in range(i+1, l):
-		if arr[i] > arr[j]:
-			arr[j], arr[i] = arr[i], arr[j]
 
+#Time complexity O(n^2), space O(1)
+
+arr = [64,2,15,3,2,1,2,3,5,7,8,4454,45]
+
+cont = True
+
+while cont:
+	cont = False
+	for i in range(len(arr)-1):
+		if arr[i] > arr[i+1]:
+			cont = True
+			arr[i], arr[i+1] = arr[i+1], arr[i]
 print(arr)
-			
